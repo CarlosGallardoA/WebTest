8890e4ed1d55fa320ac2e23b0c3ad25e8efa4079
Carlos Enrique Gallardo Ambrosio <gallardoc@truenorth.pr>
2023-10-24 17:57:45 -0500

    Add project files.

---
+++ WebTest/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebTest.Models;
+
+namespace WebTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Sumar([FromBody] SumaRequest request)
+        {
+            int suma = request.Numero1 + request.Numero2 + request.Numero3;
+            var resultado = new { suma = suma };
+            return Json(resultado);
+        }
+
+        public class SumaRequest
+        {
+            public int Numero1 { get; set; }
+            public int Numero2 { get; set; }
+            public int Numero3 { get; set; }
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
